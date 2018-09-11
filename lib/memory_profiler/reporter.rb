b868bf58a1cdd0fdef7aee7655e008ab99d23c46
william tabi <william.tabi@intercom.io>
2018-09-11 18:38:51 +0100

    GC.start

--- lib/memory_profiler/reporter.rb
+++ lib/memory_profiler/reporter.rb
@@ -46,6 +46,7 @@ module MemoryProfiler
 
       GC.enable
       GC.start
+      GC.start
 
       # Caution: Do not allocate any new Objects between the call to GC.start and the completion of the retained
       #          lookups. It is likely that a new Object would reuse an object_id from a GC'd object.
